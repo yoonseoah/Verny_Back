af7bd54d695b3ebc82812f623e83c4925c5b239b
yewon <shimyewon@ewhain.net>
2023-08-07 12:32:18 +0900

    merge

--- Hackathon3/main/admin.py
+++ Hackathon3/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from main.models import *
-
+from account.models import *
 # Register your models here.
 
 admin.site.register(Post)
--- Hackathon3/main/views.py
+++ Hackathon3/main/views.py
@@ -14,30 +14,25 @@ class PostListView(views.APIView):
     # 게시글 목록 view 부분. 조회, 작성. 지금 보니까 api명세서에 POST 메소드 추가 필요한 것 같아요. 의견 주세요!
 
     def get(self, request):
-        order_by = request.query_params.get("order_by")
-        type_filter = request.query_params.get("type")
+        order_by = request.query_params.get('order_by')
+        type_filter = request.query_params.get('type')
 
         queryset = Post.objects.all()
 
-        if order_by == "latest":  # 최신순으로
-            queryset = queryset.order_by("-created_at")
-        elif order_by == "most_scrapped":  # 스크랩 많은 순으로
-            queryset = queryset.annotate(scraps_count=Count("scraps")).order_by(
-                "-scraps_count"
-            )
-        elif order_by == "most_commented":  # 댓글 많은 순으로
-            queryset = queryset.annotate(comments_count=Count("comments")).order_by(
-                "-comments_count"
-            )
-
+        if order_by == 'latest': #최신순으로
+            queryset = queryset.order_by('-created_at')
+        elif order_by == 'most_scrapped': #스크랩 많은 순으로
+            queryset = queryset.annotate(scraps_count=Count('scraps')).order_by('-scraps_count')
+        elif order_by == 'most_commented': #댓글 많은 순으로
+            queryset = queryset.annotate(comments_count=Count('comments')).order_by('-comments_count')
+        
         if type_filter:
             queryset = queryset.filter(type=type_filter)
 
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # http://your-domain/main/posts/?order_by=most_scrapped
-    # http://127.0.0.1:8000/main/posts/?type=고전미술, 현대미술
+#http://your-domain/main/posts/?order_by=most_scrapped
+#http://127.0.0.1:8000/main/posts/?type=고전미술, 현대미술
 
     def post(self, request, format=None):  # 게시글 작성 POST 메소드입니다!
         serializer = PostSerializer(data=request.data)
@@ -55,6 +50,8 @@ class PostDetailView(views.APIView):  # 작품 해설(detail) 조회
 
 
 class CommentView(views.APIView):  # 댓글 조회, 작성
+
+
     def get(self, request, pk, format=None):
         comment = Comment.objects.filter(post_id=pk)
         serializer = CommentSerializer(comment, many=True)
@@ -66,7 +63,7 @@ class CommentView(views.APIView):  # 댓글 조회, 작성
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
-
+    
 
 class CommentDetailView(views.APIView):  # 댓글 수정,삭제
     def put(self, request, comment_pk, format=None):
@@ -116,7 +113,7 @@ class PostScrapView(views.APIView):
     def get(self, request, post_id):
         post = get_object_or_404(Post, pk=post_id)
         scraped_by_user = request.user in post.scraps.all()
-        return Response({"scraped": scraped_by_user})
+        return Response({'scraped': scraped_by_user})
 
     def post(self, request, post_id):
         post = get_object_or_404(Post, pk=post_id)
@@ -129,31 +126,29 @@ class PostScrapView(views.APIView):
             post.scraps.add(user)
             scraped = True
 
-        return Response({"scraped": scraped, "post": serializers.data})
-
+        return Response({'scraped':scraped, 'post': serializers.data})
 
 class SearchView(views.APIView):
     def get(self, request):
         queryset = Post.objects.all()
-        search_query = request.query_params.get("q")
+        search_query = request.query_params.get('q')
 
         if search_query:
             queryset = queryset.filter(
-                Q(content__icontains=search_query)
-                | Q(title__icontains=search_query)
-                | Q(painter__icontains=search_query)
+                Q(content__icontains=search_query) |  
+                Q(title__icontains=search_query) |
+                Q(painter__icontains=search_query)
             )
-        serializer = PostSerializer(queryset, many=True)
+        serializer = PostSerializer(queryset,many=True)
         return Response(serializer.data)
-
-    # http://your-domain/main/search/?q=search-keyword
+    #http://your-domain/main/search/?q=search-keyword
 
 
 class CommentLikeView(views.APIView):
     def get(self, request, comment_id):
         comment = get_object_or_404(Comment, pk=comment_id)
         liked_by_user = request.user in comment.likes.all()
-        return Response({"liked": liked_by_user})
+        return Response({'liked': liked_by_user})
 
     def post(self, request, comment_id):
         comment = get_object_or_404(Comment, pk=comment_id)
@@ -166,14 +161,13 @@ class CommentLikeView(views.APIView):
             comment.likes.add(user)
             liked = True
 
-        return Response({"liked": liked, "comment": serializers.data})
-
-
+        return Response({'liked':liked, 'comment': serializers.data})
+    
 class RecommentLikeView(views.APIView):
     def get(self, request, recomment_id):
         recomment = get_object_or_404(Recomment, pk=recomment_id)
         reliked_by_user = request.user in recomment.relikes.all()
-        return Response({"reliked": reliked_by_user})
+        return Response({'reliked': reliked_by_user})
 
     def post(self, request, recomment_id):
         recomment = get_object_or_404(Recomment, pk=recomment_id)
@@ -186,4 +180,4 @@ class RecommentLikeView(views.APIView):
             recomment.relikes.add(user)
             reliked = True
 
-        return Response({"reliked": reliked, "recomment": serializers.data})
+        return Response({'reliked':reliked, 'recomment': serializers.data})
