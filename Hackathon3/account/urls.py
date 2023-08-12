9bf1a5db775f49bc77d6ed90122d43e1f442eeea
yoonseoah <emma.yoon2002@gmail.com>
2023-08-12 23:40:59 +0900

    [feat] 댓글, 대댓글 조회 구현

--- Hackathon3/account/urls.py
+++ Hackathon3/account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup/', SignUpView.as_view()),
     path('login/', LoginView.as_view()),
     path('mypage/', ProfileView.as_view()),
+    path('mypage/commentlist/', CommentListView.as_view()),
+    path('mypage/recommentlist/', RecommentListView.as_view()),
 ]
--- Hackathon3/account/views.py
+++ Hackathon3/account/views.py
@@ -7,6 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import *
 from .serializers import *
+from main.models import *
+from main.serializers import *
 
 # Create your views here.
 
@@ -55,3 +57,53 @@ class ProfileView(views.APIView):
             serializer.save()
             return Response({'message': '프로필 업데이트 성공', 'data': serializer.data}, status=HTTP_200_OK)
         return Response({'message': '프로필 업데이트 실패', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
+
+class CommentListView(views.APIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        id = request.GET.get('id')
+        author = request.GET.get('author')
+        content = request.GET.get('content')
+        created_at = request.GET.get('created_at')
+        likes_count = request.GET.get('likes_count')
+        recomments_count = request.GET.get('recomments_count')
+
+        params = {'id': id, 'author': author, 'content': content, 'created_at': created_at, 'likes_count': likes_count, 'recomments_count': recomments_count}
+        arguments = {}
+        for key, value in params.items():
+            if value:
+                arguments[key] = value
+
+        comments = (Comment.objects.filter(author=user.id)&(Comment.objects.filter(**arguments))).distinct()
+
+        serializer = self.serializer_class(comments, many=True)
+        return Response({'message': "작성 댓글 목록 조회 성공", 'data': serializer.data}, status=HTTP_200_OK)
+
+class RecommentListView(views.APIView):
+    serializer_class = RecommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        id = request.GET.get('id')
+        author = request.GET.get('author')
+        content = request.GET.get('content')
+        created_at = request.GET.get('created_at')
+        relikes_count = request.GET.get('relikes_count')
+
+        params = {'id': id, 'author': author, 'content': content, 'created_at': created_at, 'relikes_count': relikes_count}
+        arguments = {}
+        for key, value in params.items():
+            if value:
+                arguments[key] = value
+
+        recomments = (Recomment.objects.filter(author=user.id)&(Recomment.objects.filter(**arguments))).distinct()
+
+        serializer = self.serializer_class(recomments, many=True)
+        return Response({'message': "작성 대댓글 목록 조회 성공", 'data': serializer.data}, status=HTTP_200_OK)
+
